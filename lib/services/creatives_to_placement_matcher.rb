0642cf4e67a433b12338d5be79b46f50e632a18f
Mario Miklos <mario.miklos@smartpension.co.uk>
2024-02-09 21:47:27 +0100

    Add service to match creatives to placements

---
+++ lib/services/creatives_to_placement_matcher.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreativesToPlacementMatcher
+
+  def initialize(creatives:, placement:)
+    @creatives = creatives
+    @placement = placement
+  end
+
+  def call
+    { placement: creatives.select { |creative| creative.price_in_euros >= placement.floor_in_euros } }
+  end
+
+  attr_reader :creatives, :placement
+end
---
+++ spec/services/creatives_to_placement_matcher_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe CreativesToPlacementMatcher do
+  subject do
+    described_class.new(creatives: creatives, placement: placement).call
+  end
+
+  let(:creatives) { [creative_1, creative_2, creative_3] }
+  let(:creative_1) { Creative.new(id: 'video-1', price: BigDecimal('7'), currency: 'EUR') }
+  let(:creative_2) { Creative.new(id: 'video-2', price: BigDecimal('10'), currency: 'USD') }
+  let(:creative_3) { Creative.new(id: 'video-3', price: BigDecimal('13'), currency: 'EUR') }
+  let(:placement) { Placement.new(id: 'plc-1', floor: BigDecimal('7.5'), currency: 'EUR') }
+
+  describe '#call' do
+    it 'returns the file content' do
+      expect(subject).to eq(
+        placement: [creative_2, creative_3]
+      )
+    end
+  end
+end
